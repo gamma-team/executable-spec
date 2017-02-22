afd5e4b5737c2f9215f7ce8df3e9e739877808cb
Antony Gillette <agillette@hcs.ufl.edu>
2017-02-21 21:02:49 -0500

    ip: Update component declarations for new spec
    To be FIFO-friendly, similar to udp 3 commits back

--- ip/ip_rx_component.vhd
+++ ip/ip_rx_component.vhd
@@ -1,4 +1,4 @@
-COMPONENT ip_rx
+COMPONENT ip_rx IS
     GENERIC (
         -- Input and output bus width in bytes, must be a power of 2
         width : POSITIVE := 8
@@ -8,51 +8,40 @@ COMPONENT ip_rx
         Clk : IN STD_LOGIC;
         Rst : IN STD_LOGIC;
         -- Data input bus for data from the MAC.
+        -- Byte offsets (all integer types are big endian):
+        -- 0: IP version and header length (1 byte)
+        -- 2: Total packet length (2 bytes)
+        -- 9: Protocol (1 byte)
+        -- 10: Header checksum (2 bytes)
+        -- 12: Source IP address (4 bytes)
+        -- 16: Destination IP address (4 bytes)
+        -- 20: IP datagram's data section (if IP header length field = 5)
         Data_in : IN STD_LOGIC_VECTOR(width * 8 - 1 DOWNTO 0);
-        -- Assertion indicates which Data_in bytes are valid. If there are
-        -- valid lanes, they will always be consecutive and start with the
-        -- first lane. E.g., 0b00111 or 0b11111, but never 0b00110 or 0b01010.
-        -- Additionally, the maximum amount of valid data will always be
-        -- given, that is, it is only possible for partially valid data on the
-        -- last data cycle.
+        -- Assertion indicates which Data_in bytes are valid.
         Data_in_valid : IN STD_LOGIC_VECTOR(width - 1 DOWNTO 0);
-        -- Assertion indicates that the first data is available on Data_in.
+        -- Asserted when the first data is available on Data_in.
         Data_in_start : IN STD_LOGIC;
         -- Asserted when the last valid data is available on Data_in.
         Data_in_end : IN STD_LOGIC;
-        -- IP version and packet length. Valid when Data_in_start is asserted.
-        Ver_len_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Identification and fragmentation info. Valid when Data_in_start is asserted.
-        Id_frag_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- TTL, protocol, and checksum. Valid when Data_in_start is asserted.
-        Ttl_proto_cksm_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Source IP address. Valid when Data_in_start is asserted.
-        Addr_src_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Destination IP address. Valid when Data_in_start is asserted.
-        Addr_dst_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        -- Indicate that there has been an error in the current data stream.
+        -- Data_in will be ignored until the next Data_in_start assertion.
+        Data_in_err : IN STD_LOGIC;
 
-        -- IPv4 payload data output bus to the UDP module. Valid byte
-        -- lanes depend on Data_out_valid.
+        -- IPv4 payload data output bus to the UDP module.
+        -- Byte offsets (all integer types are big endian):
+        -- 0: Source IP address
+        -- 4: Destination IP address
+        -- 8: Protocol
+        -- 9: IP datagram's data section
         Data_out : OUT STD_LOGIC_VECTOR(width * 8 - 1 DOWNTO 0);
-        -- Assertion indicates which Data_out bytes are valid. If there are
-        -- valid lanes, they will always be consecutive and start with the
-        -- first lane. E.g., 0b00111 or 0b11111, but never 0b00110 or 0b01010.
-        -- Additionally, the maximum amount of valid data will always be
-        -- given, that is, it is only possible for partially valid data on the
-        -- last data cycle.
+        -- Assertion indicates which Data_out bytes are valid.
         Data_out_valid : OUT STD_LOGIC_VECTOR(width - 1 DOWNTO 0);
-        -- Assertion indicates that the first data is available on Data_out.
+        -- Asserted when the first data is available on Data_out.
         Data_out_start : OUT STD_LOGIC;
         -- Asserted when the last data is available on Data_out.
         Data_out_end : OUT STD_LOGIC;
         -- Indicate that there has been an error in the current datagram.
         -- Data_out should be ignored until the next Data_out_start assertion.
-        Data_out_err : OUT STD_LOGIC;
-        -- Source IP address. Valid when Data_out_start is asserted.
-        Addr_src_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Destination IP address. Valid when Data_out_start is asserted.
-        Addr_dst_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Protocol type. Valid when Data_out_start is asserted.
-        Proto_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+        Data_out_err : OUT STD_LOGIC
     );
 END COMPONENT;
--- ip/ip_tx_component.vhd
+++ ip/ip_tx_component.vhd
@@ -1,4 +1,4 @@
-COMPONENT ip_tx
+COMPONENT ip_tx IS
     GENERIC (
         -- Input and output bus width in bytes, must be a power of 2
         width : POSITIVE := 8
@@ -8,49 +8,36 @@ COMPONENT ip_tx
         Clk : IN STD_LOGIC;
         Rst : IN STD_LOGIC;
         -- Data input bus for the MAC from the UDP module.
+        -- Byte offsets (all integer types are big endian):
+        -- 0: Source IP address
+        -- 4: Destination IP address
+        -- 8: UDP datagram
         Data_in : IN STD_LOGIC_VECTOR(width * 8 - 1 DOWNTO 0);
-        -- Assertion indicates which Data_in bytes are valid. If there are
-        -- valid lanes, they will always be consecutive and start with the
-        -- first lane. E.g., 0b00111 or 0b11111, but never 0b00110 or 0b01010.
-        -- Additionally, the maximum amount of valid data will always be
-        -- given, that is, it is only possible for partially valid data on the
-        -- last data cycle.
+        -- Assertion indicates which Data_in bytes are valid.
         Data_in_valid : IN STD_LOGIC_VECTOR(width - 1 DOWNTO 0);
-        -- Assertion indicates that the first data is available on Data_in.
+        -- Asserted when the first data is available on Data_in
         Data_in_start : IN STD_LOGIC;
         -- Asserted when the last valid data is available on Data_in.
         Data_in_end : IN STD_LOGIC;
-        -- Source IP address. Valid when Data_in_start is asserted.
-        Addr_src_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Destination IP address. Valid when Data_in_start is asserted.
-        Addr_dst_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        -- IPv4 output bus to the MAC. Valid byte lanes depend on
-        -- Data_out_valid.
+        -- IPv4 output bus to the MAC.
+        -- Byte offsets (all integer types are big endian):
+        -- 0: IP version and header length (1 byte)
+        -- 2: Total packet length (2 bytes)
+        -- 9: Protocol (1 byte)
+        -- 10: Header checksum (2 bytes)
+        -- 12: Source IP address (4 bytes)
+        -- 16: Destination IP address (4 bytes)
+        -- 20: IP datagram's data section
         Data_out : OUT STD_LOGIC_VECTOR(width * 8 - 1 DOWNTO 0);
-        -- Assertion indicates which Data_out bytes are valid. If there are
-        -- valid lanes, they will always be consecutive and start with the
-        -- first lane. E.g., 0b00111 or 0b11111, but never 0b00110 or 0b01010.
-        -- Additionally, the maximum amount of valid data will always be
-        -- given, that is, it is only possible for partially valid data on the
-        -- last data cycle.
+        -- Assertion indicates which Data_out bytes are valid.
         Data_out_valid : OUT STD_LOGIC_VECTOR(width - 1 DOWNTO 0);
-        -- Assertion indicates that the first data is available on Data_out.
+        -- Asserted when the first data is available on Data_out.
         Data_out_start : OUT STD_LOGIC;
         -- Asserted when the last data is available on Data_out.
         Data_out_end : OUT STD_LOGIC;
         -- Indicate that there has been an error in the current datagram.
         -- Data_out should be ignored until the next Data_out_start assertion.
-        Data_out_err : OUT STD_LOGIC;
-        -- IP version and packet length. Valid when Data_out_start is asserted.
-        Ver_len_out : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Identification and fragmentation info. Valid when Data_out_start is asserted.
-        Id_frag_out : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- TTL, protocol, and checksum. Valid when Data_out_start is asserted.
-        Ttl_proto_cksm_out : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Source IP address. Valid when Data_out_start is asserted.
-        Addr_src_out : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- Destination IP address. Valid when Data_out_start is asserted.
-        Addr_dst_out : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Data_out_err : OUT STD_LOGIC
     );
 END COMPONENT;
